c985cfbe021cdcc985b721294ff3aafe3adcb261
Aung Phyo Pyae Kyaw <aungphyopyaekyaw@gmail.com>
2019-01-08 12:50:49 +0630

    Updates

---
+++ precache-manifest.8f6dd6b62afc062231772a22c8f29666.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c8be48c4ce5613eaed35",
+    "url": "/static/js/main.c8be48c4.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "c8be48c4ce5613eaed35",
+    "url": "/static/css/main.788903f2.chunk.css"
+  },
+  {
+    "revision": "02a53819a4bcbabd1f3578985e654f1e",
+    "url": "/index.html"
+  }
+];
